3ae96d2784fc0a438c07f3dbb8dc9699f12bc349
S-Khan786 <osahilkhan654@gmail.com>
2025-03-11 10:35:50 +0530

    Completed The Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,3 @@
-// // src/App.jsx
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home";
-// import Events from "./pages/Events";
-// import About from './pages/About';
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
-// import "./index.css";
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Header />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/events" element={<Events />} />
-//         <Route path="/about" element={<About />} />
-//       </Routes>
-//       <Footer />
-//     </Router>
-//   );
-// };
-
-// export default App;
-
-// src/App.jsx
 import React from "react";
 import {
   BrowserRouter as Router,
